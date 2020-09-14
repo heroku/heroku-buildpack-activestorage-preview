9056773b88c77ff33ff4900f9bb116c41553d5ed
Richard Schneeman <richard.schneeman+foo@gmail.com>
2020-09-14 14:27:38 -0500

    [changelog skip] Add tests (#19)
    * Initial tests
    
    * Add active storage specific tests

---
+++ spec/hatchet/buildpack_spec.rb
@@ -0,0 +1,24 @@
+require_relative "../spec_helper.rb"
+
+RSpec.describe "This buildpack" do
+  it "has its own tests" do
+    # Specify where you want your buildpack to go using :default
+    buildpacks = [:default, "heroku/ruby"]
+
+    # To deploy a different app modify the hatchet.json or
+    # commit an app to your source control and use a path
+    # instead of "default_ruby" here
+    Hatchet::Runner.new("default_ruby", buildpacks: buildpacks).tap do |app|
+      app.before_deploy do
+        # Modfiy the app here if you need
+      end
+      app.deploy do
+        # Assert the behavior you desire here
+        expect(app.output).to match("deployed to Heroku")
+
+        expect(app.run("which ffmpeg")).to match("/app/.heroku/activestorage-preview/usr/bin/ffmpeg")
+        expect(app.run("which pdftoppm")).to match("/app/.heroku/activestorage-preview/usr/bin/pdftoppm")
+      end
+    end
+  end
+end
